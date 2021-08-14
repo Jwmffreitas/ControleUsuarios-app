b680c1a81d967800c56e680b57afe7bf38043133
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-08-14 18:42:59 -0300

    Instalando faker e criando usuários no db

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,24 @@
 const { Router } = require('express')
 const router = Router()
 
-router.get('/api/users', (req, res) => {
-    res.json('User List')
+const User = require('../models/user')
+
+const faker = require('faker')
+
+router.get('/api/users', async (req, res) => {
+    const users = await User.findAll()
+    res.json({users})
+})
+
+router.get('/api/users/create', async (req, res) => {
+    for(let i = 0; i < 5; i++) {
+        await User.create({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            avatar: faker.image.avatar()
+        })
+    }
+    res.json({message: '5 Users Created'})
 })
 
 module.exports = router
