ad929b2c7c2b1bf7c26aa68ffbcaff3399d9fc3d
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-08-14 11:40:53 -0300

    Server rodando

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 5000
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(port, function() {
+    console.log(`Server runing on port ${port}`)
+});
+
+
 (async () => {
 
     const database = require('./db');
