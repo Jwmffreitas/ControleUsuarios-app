83ba75133c9a84f07445c614c454031235fb9e70
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-11-04 12:10:22 -0300

    Pegando dados da api

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
@@ -12,11 +15,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  getUsers() async {
+    http.Response response =
+        await http.get(Uri.parse('http://192.168.1.8:5000/api/users'));
+    debugPrint(response.body);
+    json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUsers();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Users List'),
+        backgroundColor: Colors.indigo[900],
       ),
+      //body: ListView.builder(),
     );
   }
 }
